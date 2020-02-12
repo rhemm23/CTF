5c41a81d5d307731be6095830d76fc41dfd30688
Your Name <ryanhemmila@gmail.com>
2020-02-12 07:08:01 -0600

    updates

--- com/ryan/ctf/World.java
+++ com/ryan/ctf/World.java
@@ -2,31 +2,47 @@ package com.ryan.ctf;
 
 import com.ryan.ctf.blocks.Block;
 
+/*
+ * The reality of the player object
+ */
 public class World {
 
   private static final int DEFAULT_WORLD_WIDTH = 256;
   private static final int DEFAULT_WORLD_HEIGHT = 256;
 
+  private static final float DEFAULT_WORLD_GRAVITY = 0.0042f;
+
   private Block[][] _blocks;
+
+  private float _gravity;
 
   private int _height;
   private int _width;
 
   public World() {
     this(DEFAULT_WORLD_WIDTH, DEFAULT_WORLD_HEIGHT);
+
   }
 
   public World(int width, int height) {
     this._blocks = new Block[height][width];
+    this._gravity = DEFAULT_WORLD_GRAVITY;
     this._height = height;
     this._width = width;
   }
 
   /*
-   * Gets the block position as specific coordinates
+   * Gets the block at the specified position
    */
   public Block getBlock(int x, int y) {
     return this._blocks[y][x];
+  }
+
+  /*
+   * Sets the block at the specified position
+   */
+  public void setBlock(int x, int y, Block block) {
+    this._blocks[y][x] = block;
   }
 
   public int getHeight() {
@@ -35,5 +51,13 @@ public class World {
 
   public int getWidth() {
     return this._width;
+  }
+
+  public void setGravity(float gravity) {
+    this._gravity = gravity;
+  }
+
+  public float getGravity() {
+    return this._gravity;
   }
 }
