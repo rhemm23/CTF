706b166fdcb891dce70cc047df4d2b6e4f5bd73b
Your Name <ryanhemmila@gmail.com>
2020-02-12 06:32:43 -0600

    drawer updates

--- com/ryan/ctf/drawers/WorldDrawer.java
+++ com/ryan/ctf/drawers/WorldDrawer.java
@@ -1,12 +1,14 @@
 package com.ryan.ctf.drawers;
 
 import com.ryan.ctf.World;
-import com.ryan.ctf.blocks.Block;
 import com.ryan.ctf.math.Rectanglei;
 
+/*
+ * Draws specified blocks of a world
+ */
 public class WorldDrawer implements IDrawable {
 
-  private TextureDrawer[][] _blockDrawers;
+  private BlockDrawer[][] _blockDrawers;
   private Rectanglei _renderBounds;
   private World _world;
 
@@ -22,15 +24,13 @@ public class WorldDrawer implements IDrawable {
     this._world = world;
 
     // Update drawer array size
-    this._blockDrawers = new TextureDrawer[world.getHeight()][world.getWidth()];
+    this._blockDrawers = new BlockDrawer[world.getHeight()][world.getWidth()];
 
     // Build texture drawers
     for(int y = 0; y < world.getHeight(); y++) {
       for(int x = 0; x < world.getWidth(); x++) {
-        Block block = this._world.getBlock(x, y);
-        if(block != null) {
-          this._blockDrawers[y][x] = new TextureDrawer(
-              block.getX(), block.getY(), block.getTexture());
+        if(this._world.getBlock(x, y) != null) {
+          this._blockDrawers[y][x] = new BlockDrawer(this._world.getBlock(x, y));
         }
       }
     }
@@ -56,9 +56,8 @@ public class WorldDrawer implements IDrawable {
   public void draw(float[] viewProjectionMatrix) {
     for(int y = this._renderBounds.getY1(); y < this._renderBounds.getY2(); y++) {
       for(int x = this._renderBounds.getX1(); x < this._renderBounds.getX2(); x++) {
-        TextureDrawer textureDrawer = this._blockDrawers[y][x];
-        if(textureDrawer != null) {
-          textureDrawer.draw(viewProjectionMatrix);
+        if(this._blockDrawers[y][x] != null) {
+          this._blockDrawers[y][x].draw(viewProjectionMatrix);
         }
       }
     }
